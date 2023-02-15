af0731b6fabfb611b07fb75148f83c3dea22a53e
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-15 14:15:31 -0600

    [change]: useGetCourses refactor

--- src/components/courses/useGetCourses.js
+++ src/components/courses/useGetCourses.js
@@ -2,30 +2,23 @@ import { useState, useEffect } from 'react'
 
 export const useGetCourses = ()=>{
     const baseUrl = 'https://courses-rest-api-hospital.vercel.app/api/courses/all-courses'
-    const [courses, setCourses]= useState({
-        data: [],
-        loading: true,
-    })
-    useEffect(()=>{
-        (async()=>{
-            try {
-                const response = await fetch(baseUrl)
-                const json = await response.json()
-                if(json.length > 0){
-                    setCourses({
-                        data: json,
-                        loading:false
-                    })
-                }else{
-                    setCourses({
-                        data: [],
-                        loading:true
-                    })
-                }
-            } catch (error) {
-                throw new Error('Error al cargar la información')
+    const [courses, setCourses]= useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const getCourses= async()=>{
+        try {
+            const response = await fetch(baseUrl)
+            const json = await response.json()
+            if(response.status === 200){
+                setIsLoading(false)
             }
-        })()
+            console.log(json)
+            return setCourses(json)
+        } catch (error) {
+            throw new Error('Error alcargar la información')
+        }
+    }
+    useEffect(()=>{
+        getCourses()
     },[])
-    return [courses]
+    return [courses, isLoading]
 }
