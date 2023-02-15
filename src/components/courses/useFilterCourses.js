1af11e690e2b00ecdfa0b74a45b48ff298786ed6
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-15 08:34:47 -0600

    [add]: useMemo in filtered data

--- src/components/courses/useFilterCourses.js
+++ src/components/courses/useFilterCourses.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { months } from "./months"
 
 export const useFilterCourses = (courses = [])=>{
     const [selectedMonth, setSelectedMonth]= useState(months[0])
-    let filteredCourses = courses.filter(course=> course.month_id === selectedMonth.id)
+    let filteredCourses = useMemo(()=>{
+        return courses.filter(course=> course.month_id === selectedMonth.id)
+    },[courses])
     return [filteredCourses, setSelectedMonth]
 }
