dae4034bfc09247453f227254323532c3b2e0f9e
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-18 15:30:19 -0600

    [add]: Sweet alert and used it in Card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,45 @@
 import { memo } from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import 'animate.css';
 import Image from 'next/image'
 import styles from './card.module.css'
 
+const MySwal = withReactContent(Swal)
+
 const Card = ({image, title})=>{
-    return (
-        <article className={styles.cardWrapper}>
+    const showModalInfo = ()=>{
+        MySwal.fire({
+            html: 
+            <article className={styles.cardWrapper}>
+            <figure className={styles.cardImage}>
+            <Image
+                src={!image ?'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676678760/banner-80_zhlxd1.webp'
+                :image
+                }
+                loading='lazy'
+                alt='No se pudo cargar la imágen'
+                width={1000}
+                height={150}
+                quality
+                />
+            </figure>
+            <div className={styles.cardTitles}>
+                <p>{title}</p>
+            </div>
+            </article>,
+            width: 800,
+            showConfirmButton:false,
+            showClass: {
+                popup: 'animate__animated animate__fadeIn'
+              },
+              hideClass: {
+                popup: 'animate__animated animate__fadeOut'
+              }
+        })
+    }
+   return (
+        <article className={styles.cardWrapper} onClick={showModalInfo}>
             <figure className={styles.cardImage}>
             <Image
                     src={
