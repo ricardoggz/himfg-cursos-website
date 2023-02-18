d2efc2fffe7817db5ec97f40704a2726436b88cc
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-18 14:14:20 -0600

    [change]: Use axios to do fetch

--- src/components/courses/useGetCourses.js
+++ src/components/courses/useGetCourses.js
@@ -1,14 +1,15 @@
+import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export const useGetCourses = ()=>{
-    const baseUrl = 'https://courses-rest-api-hospital.vercel.app/api/courses/all-courses'
+    const baseUrl = 'http://localhost:3030/api/courses/all-courses'
     const [courses, setCourses]= useState([])
     const [isLoading, setIsLoading] = useState(true)
     const getCourses= async()=>{
         try {
-            const response = await fetch(baseUrl)
-            const json = await response.json()
-            if(response.status === 200){
+            const response = await axios.get(baseUrl)
+            const json = response.data
+            if(json){
                 setIsLoading(false)
             }
             return setCourses(json)
