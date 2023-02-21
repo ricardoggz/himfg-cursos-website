d18be3c2122903f2910bcb0fbf48b172f3d42119
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-20 18:55:52 -0600

    [change]: Props in Card Component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,45 +1,11 @@
 import { memo } from 'react'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 import 'animate.css';
 import Image from 'next/image'
 import styles from './card.module.css'
 
-const MySwal = withReactContent(Swal)
-
-const Card = ({image, title})=>{
-    const showModalInfo = ()=>{
-        MySwal.fire({
-            html: 
-            <article className={styles.cardWrapper}>
-            <figure className={styles.cardImage}>
-            <Image
-                src={!image ?'https://res.cloudinary.com/diuxbqmn5/image/upload/v1676678760/banner-80_zhlxd1.webp'
-                :image
-                }
-                loading='lazy'
-                alt='No se pudo cargar la imágen'
-                width={1000}
-                height={150}
-                quality
-                />
-            </figure>
-            <div className={styles.cardTitles}>
-                <p>{title}</p>
-            </div>
-            </article>,
-            width: 800,
-            showConfirmButton:false,
-            showClass: {
-                popup: 'animate__animated animate__fadeIn'
-              },
-              hideClass: {
-                popup: 'animate__animated animate__fadeOut'
-              }
-        })
-    }
+const Card = ({image, title, showModal, children})=>{
    return (
-        <article className={styles.cardWrapper} onClick={showModalInfo}>
+        <article className={styles.cardWrapper} onClick={showModal}>
             <figure className={styles.cardImage}>
             <Image
                     src={
@@ -58,6 +24,10 @@ const Card = ({image, title})=>{
             <div className={styles.cardTitles}>
                 <p>{title}</p>
             </div>
+            <div className={styles.cardButtons}>
+                {children}
+            </div>
+           
         </article>
     )
 }
