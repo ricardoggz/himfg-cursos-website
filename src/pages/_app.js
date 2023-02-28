322ca099df6d47f5fc9e111b49a2b6f706149a94
ricardoggz <ricardo_ggzicm@outlook.com>
2023-02-28 14:51:55 -0600

    [add]: Layout component to router

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/global.css'
 import { Montserrat } from '@next/font/google'
+import { Layout } from '../components'
+
 const montserratFont = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={montserratFont.className}>
-      <Component {...pageProps} />
-    </main>
+    <Layout>
+      <main className={montserratFont.className}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
   )
 }
