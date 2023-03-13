b84d743e853db1b39e2da05891761cf9bb976cf4
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-13 13:33:32 -0600

    [add]: useOnChange to input states

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,13 +1,39 @@
+import axios from "axios"
 import { Login, Container } from "../../components"
+import { useOnChange } from "../../hooks"
 
 const Admin = ()=> {
+    const [inputData, onChange, onReset]= useOnChange()
+    const login = async(evt)=>{
+        evt.preventDefault()
+        try {
+            await axios.post(
+                `${process.env.BASE_URL_API}api/auth/login-admin`,
+                inputData
+                )
+        } catch (error) {
+            throw new Error(error)
+        }
+        evt.target.reset()
+        onReset()
+    }
     return (
         <Container>
-            <Login>
+            <Login onSubmit={login}>
                 <label>Usuario:</label>
-                <input type='text' required/>
+                <input
+                type='text'
+                required
+                onChange={onChange}
+                name='admin_user'
+                />
                 <label>Contraseña:</label>
-                <input type='password' required/>
+                <input
+                type='password'
+                required
+                onChange={onChange}
+                name='admin_password'
+                />
                 <button>Iniciar sesión</button>
             </Login>
         </Container>
