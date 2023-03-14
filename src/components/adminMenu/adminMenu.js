c9e3a9068851bbd32d7aa5b5916d5b3bca6d6d29
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-14 11:53:52 -0600

    [change]: AdminMenu styles

--- src/components/adminMenu/adminMenu.js
+++ src/components/adminMenu/adminMenu.js
@@ -1,5 +1,22 @@
+import Link from 'next/link'
+import { GridContainer } from '../../components'
 import styles from './adminMenu.module.css'
 
 export const AdminMenu =()=>{
-
+    return (
+        <GridContainer>
+            <Link
+                className={`${styles.menuOption} boxShadow`}
+                href='/admin/dashboard'
+            >
+                    Cursos
+            </Link>
+            <Link
+                className={`${styles.menuOption} boxShadow`}
+                href='/admin/dashboard'
+            >
+                    Estudiantes
+            </Link>
+        </GridContainer>
+    )
 }
