d421ebbacc41ce6a4e04eea50f20c6ec1499473d
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-16 14:21:31 -0600

    [add]: Admin provider wrapper component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/global.css'
 import { Montserrat } from '@next/font/google'
 import { Layout } from '../components'
+import { AdminProvider } from '../contexts'
 
 const montserratFont = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <main className={montserratFont.className}>
-        <Component {...pageProps} />
-      </main>
-    </Layout>
+    <AdminProvider>
+      <Layout>
+        <main className={montserratFont.className}>
+          <Component {...pageProps} />
+        </main>
+      </Layout>
+    </AdminProvider>
   )
 }
