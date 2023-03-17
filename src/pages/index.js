cd5cffc4cf6d09c9f92fd2cac7c8e82cd6bc7462
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-17 13:53:31 -0600

    [add]: Jumbotron component in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Courses, Container, Title } from '../components'
+import { Courses, Container, Title, Jumbotron } from '../components'
 
 export default function Home() {
   return (
@@ -10,6 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://res.cloudinary.com/diuxbqmn5/image/upload/v1677114497/himfg-logo_ewzx59.webp" />
       </Head>
+      <Jumbotron />
       <Container>
         <Title>Oferta académica</Title>
         <Courses />
