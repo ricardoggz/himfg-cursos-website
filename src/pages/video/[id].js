597667e1244cbcf03c569c654708b8092a1df848
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-25 11:27:59 -0600

    [add]: handling null values in path video[id]

--- src/pages/video/[id].js
+++ src/pages/video/[id].js
@@ -1,50 +1,14 @@
 import { useRouter } from "next/router"
-import { useFetch } from "../../hooks"
-import { Container, GridContainer, Loader, Login } from '../../components'
+import { Container, GridContainer } from '../../components'
 
-const Videos = ({folderNumber, liveVideo})=>{
-    const config = {
-        headers: { Authorization: `Bearer 586637bf90ea7727edc8c90c95b056c3` }
-    }
-    const [loading, data]= useFetch({
-        url: `https://api.vimeo.com/me/folders/${folderNumber}/videos`,
-        config: config
-    })
-    return (
-        <>
-            {
-            !loading?
-            <GridContainer>
-                {
-                    data.data.data.map((video, i)=>(
-                        <div key={i} className="boxShadow">
-                            <iframe
-                            src={video.player_embed_url}
-                            frameBorder="0"
-                            allow="autoplay; fullscreen; picture-in-picture"
-                            allowFullScreen
-                            />
-                            <span>{video.name}</span>
-                        </div>    
-                    ))
-                }
-            </GridContainer>
-            :
-            <Loader message='Cargando videos'/>
-            }
-        </>
-    )
-}
 const Video = ({data, title})=>{
     const router = useRouter()
-    //const videos = data.map((video)=>video.player_embed_url)
-    console.log(data.data)
     return (
         <Container>
             <h1>{title}</h1>
             <GridContainer>
                {
-                data.data.map((video, i)=>(
+                data?.data?.map((video, i)=>(
                     <iframe
                         src={video.player_embed_url}
                         key={i}
@@ -67,15 +31,16 @@ export const getStaticPaths = async()=>{
     })
     return {
         paths,
-        fallback: true
+        fallback: false
     }
 }
 export const getStaticProps=async({params})=>{
+    const videoId = params.id ?? ''
     const config = {
         headers: { Authorization: `Bearer 586637bf90ea7727edc8c90c95b056c3` }
     }
     const response = await fetch(
-        `https://api.vimeo.com/me/folders/${params.id}/videos`,
+        `https://api.vimeo.com/me/folders/${videoId}/videos`,
         config
     )
     const data = await response.json()
