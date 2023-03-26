f14849015bdbc28bd36378d818ac941a053f7e84
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-26 13:16:28 -0600

    [change]: href to dynamic route

--- src/components/courses/courses.js
+++ src/components/courses/courses.js
@@ -67,7 +67,7 @@ export const Courses = ()=>{
                                         :
                                         <Link
                                             href={`/video/[...id]`}
-                                            as ={`/video/${course.course_vimeo_folder}/${course.course_name}`}
+                                            as ={`/video/${course.course_name}/${course.course_vimeo_folder}`}
                                         >
                                             Ingresar
                                         </Link>
