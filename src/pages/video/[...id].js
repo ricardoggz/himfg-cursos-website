c8fe889ef89e76ce701afebb70e359c7d738aa9d
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-25 23:23:04 -0600

    [change]: params as an array to dynamic route

--- src/pages/video/[...id].js
+++ src/pages/video/[...id].js
@@ -1,18 +1,31 @@
 import { useRouter } from "next/router"
+import styles from './video.module.css'
 import { Container, GridContainer } from '../../components'
 
-const Video = ({data, title})=>{
-    const router = useRouter()
+const Video = (props)=>{
+    useRouter()
     return (
         <Container>
-            <h1>{title}</h1>
+            <h3 className={styles.videoTitle}>
+               {props.title}
+            </h3>
             <GridContainer>
                {
-                data?.data?.map((video, i)=>(
-                    <iframe
+                props.data?.data?.map((video, i)=>(
+                    <div className={`${styles.iframeVideo} boxShadow`}>
+                        <iframe
                         src={video.player_embed_url}
                         key={i}
-                    />
+                        frameBorder='0'
+                        allow="
+                        autoplay;
+                        fullscreen;
+                        picture-in-picture
+                        "
+                        allowFullScreen
+                        />
+                        <span>{video.name}</span>
+                    </div>
                 ))
                }
             </GridContainer>
@@ -22,10 +35,11 @@ const Video = ({data, title})=>{
 export const getStaticPaths = async()=>{
     const response = await fetch(`${process.env.BASE_URL_API}api/courses/all-courses`)
     const json = await response.json()
-    const paths = json.map((course)=>{
+    const paths = json.filter((course)=>course.course_vimeo_folder)
+    .map((course)=>{
         return{
             params:{
-                id : `${course.course_vimeo_folder}`.toString()
+                 id: [`${course.course_vimeo_folder}`, `${course.course_name}`],
             }
         }
     })
@@ -40,15 +54,14 @@ export const getStaticProps=async({params})=>{
         headers: { Authorization: `Bearer 586637bf90ea7727edc8c90c95b056c3` }
     }
     const response = await fetch(
-        `https://api.vimeo.com/me/folders/${videoId}/videos`,
+        `https://api.vimeo.com/me/folders/${videoId[0]}/videos`,
         config
     )
     const data = await response.json()
-
     return {
         props:{
-            title: 'Hello World',
-            data: data
+            data: data,
+            title: videoId[1] || null
         }
     }
 }
