207fcca0efbb94a8ef65f26a4d43c7872cf63959
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-27 13:56:58 -0600

    [add]: new footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,44 @@
+import styles from './footer.module.css'
+import { BsFacebook, BsTwitter } from 'react-icons/bs'
+export const Footer = ()=>{
+    return (
+        <footer className={styles.footerWrapper}>
+            <ul className={`${styles.footerList} flexContainer`}>
+                <div>
+                    <li>Redes sociales:</li>
+                    <li>
+                        <a
+                        href='https://www.facebook.com/HospitalInfantilDeMexicoFG/?locale=es_LA'
+                        target={'_blank'}
+                        >
+                            <BsFacebook />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                        href='https://www.facebook.com/HospitalInfantilDeMexicoFG/?locale=es_LA'
+                        target={'_blank'}
+                        >
+                            <BsTwitter />
+                        </a>
+                    </li>
+                </div>
+                <div>
+                    <li>Dirección:</li>
+                    <li>
+                        Dr.Márquez No.162, Col. Doctores, Delegación Cuauhtémoc; Mexico D.F. C.P 06720
+                    </li>
+                </div>
+                <div>
+                    <li>Teléfono:</li>
+                    <li>
+                        5228 9917
+                    </li>
+                </div>
+            </ul>
+            <p className={styles.copyRight}>
+                HIMFG - Todos los derechos reservados &copy;
+            </p>
+        </footer>
+    )
+}
