176ec0f38518efa16fa0614637e51a4cda78a044
ricardoggz <ricardo_ggzicm@outlook.com>
2023-03-28 13:58:47 -0600

    [add]: new menuOptions component

---
+++ src/components/menuOptions/menuOptions.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { BsBook, BsSearch, BsPencilFill } from 'react-icons/bs'
+import { RiAdminLine, RiMicroscopeFill } from 'react-icons/ri'
+import { TbRibbonHealth } from 'react-icons/tb'
+import { GridContainer } from '../../components'
+import styles from './menuOptions.module.css'
+
+export const MenuOptions = ()=>{
+    return (
+       <GridContainer>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <BsBook />
+            <span>Dirección general</span>
+        </Link>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <TbRibbonHealth />
+            <span>Dirección médica</span>
+        </Link>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <BsPencilFill />
+            <span>Dirección de enseñanza</span>
+        </Link>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <RiMicroscopeFill />
+            <span>Dirección de investigación</span>
+        </Link>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <RiAdminLine />
+            <span>Dirección de administración</span>
+        </Link>
+        <Link href='/' className={`${styles.menuOption}`}>
+            <BsSearch />
+            <span>Dirección de planeación</span>
+        </Link>
+       </GridContainer>
+    )
+}
