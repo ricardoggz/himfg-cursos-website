e7dfa61e84e59b52740c7de5977df4cffce4f1fb
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-04 12:44:07 -0600

    [add]: ensenanza page

--- src/pages/ensenanza/index.js
+++ src/pages/ensenanza/index.js
@@ -49,6 +49,15 @@ const EducativeOfer = ()=>{
           <p>
             Una de las tareas sustantivas es evaluar los estudios de pregrado, especialidades, subespecialidades, diplomados, cursos de educación continua, con el fin de fomentar la actualización entre el personal de salud.
           </p>
+          <p>
+            Esta dirección tiene la facultad de emitir los reconocimientos, diplomas, certificados, títulos con validez oficial por la UNAM, así como otras instituciones educativas para titulación de los profesionales de salud.
+          </p>
+          <p>
+            En Enseñanza y Desarrollo Académico se realizan los mecanismos que permiten evaluar la calidad de los programas educativos y precisar su impacto en la prestación de los servicios en el Hospital.
+          </p>
+          <p>
+            La dirección tiene la labor de vigilar la participación en la docencia de los médicos, investigadores, enfermeras y otros profesionistas del hospital, así como facilitar la información bibliográfica actualizada durante el desarrollo de protocolos de investigación en el proceso de enseñanza-aprendizaje de médicos residentes, médicos adscritos e investigadores del hospital.
+          </p>
         </div>
       </Container>
     </>
