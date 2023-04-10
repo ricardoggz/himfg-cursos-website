08a692d1cb341128e5766fbe142f3dbb769e4c77
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-10 14:20:41 -0600

    [add]: clear localStorage

--- src/pages/ensenanza/offer.js
+++ src/pages/ensenanza/offer.js
@@ -1,5 +1,9 @@
+import { useEffect } from 'react'
 import { Courses, Container, Title } from '../../components'
 const CoursesList = ()=>{
+    useEffect(()=>{
+        localStorage.clear()
+    },[])
     return (
         <Container>
             <Title>Oferta educativa del HIMFG</Title>
