903c3d1c0bb4e2e8ee91dad69eda8212420d79d6
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-11 13:18:20 -0600

    [add]: User provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,21 @@
 import '../styles/global.css'
 import { Montserrat } from '@next/font/google'
 import { Layout, Footer } from '../components'
-import { AdminProvider } from '../contexts'
+import { AdminProvider, UserProvider } from '../contexts'
 
 const montserratFont = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <AdminProvider>
-      <Layout>
-        <main className={montserratFont.className}>
-          <Component {...pageProps} />
-          <Footer />
-        </main>
-      </Layout>
+      <UserProvider>
+        <Layout>
+          <main className={montserratFont.className}>
+            <Component {...pageProps} />
+            <Footer />
+          </main>
+        </Layout>
+      </UserProvider>
     </AdminProvider>
   )
 }
