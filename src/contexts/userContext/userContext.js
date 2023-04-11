2423d36951f2006f75c201e44a79a06d013ec857
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-11 13:11:45 -0600

    [add]: user context

---
+++ src/contexts/userContext/userContext.js
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react"
+
+export const UserContext = createContext()
+
+export const UserProvider = ({children})=>{
+    const [user, setUser] = useState(null)
+    const loginUser = (user)=>{
+        setUser(user)
+        localStorage.setItem('user', JSON.stringify(user))
+    }
+    const signOutUser = ()=>{
+        setUser(null)
+        localStorage.removeItem('user')
+    }
+    return (
+        <UserContext.Provider value={{user, loginUser, signOutUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
