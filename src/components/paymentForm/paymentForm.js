335710da09447517063f4f2b9ba53b8072f211a1
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-16 17:12:06 -0600

    [add]: new paymentForm component

---
+++ src/components/paymentForm/paymentForm.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react"
+
+export const PaymentForm = ()=>{
+    useEffect(()=>{
+        console.log(Payment.encrypt)
+    },[])
+    return (
+        <p>Pago</p>
+    )
+}
