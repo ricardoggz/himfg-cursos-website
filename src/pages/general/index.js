da6d1364c551cb07ee31fd857777ab01793b2e35
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-20 10:44:30 -0600

    [add]: general direction index page

---
+++ src/pages/general/index.js
@@ -0,0 +1,39 @@
+import { Container, Page } from '../../components'
+
+const General = ()=>{
+    const paths = [
+        {
+            path: '/',
+            name: 'Inicio'
+        }
+    ]
+    return (
+        <Container>
+            <Page title='Dirección general' listMenu={paths}>
+                <p>
+                    El Hospital Infantil de México tiene 78 años brindando atención pediátrica especializada a la niñez más desprotegida de nuestro país,
+                    su modelo de atención,
+                    enseñanza e investigación,
+                    han sido repetidos en los actuales Institutos Nacionales de Salud y otros hospitales del país y de América Latina.
+                </p>
+                <p>
+                    Sus recursos provienen en su mayoría de las arcas federales y atenciones subrogadas,
+                    sin embargo, no es suficiente.
+                </p>
+                <p>
+                    Nuestro Instituto es pionero de pediatría en el País en una gran cantidad de temas del desarrollo y de vanguardia como: vacunas,
+                    bacteriología, infectología, nutrición, nefrología, endocrinología, cirugía de corazón, trasplantes de corazón, riñón, etc.
+                </p>
+                <p>
+                    Ha sido siempre un icono de la Pediatría Nacional,
+                    hemos publicado más de 300 libros;
+                    múltiples artículos científicos,
+                    capítulos de libros desde hace más de 17 años tenemos el "Programa Sigamos Aprendiendo en el Hospital" que apoya a los pacientes a continuar con sus estudios estando hospitalizados tenemos una historia larga en la formación de alumnos de todas las especialidades pediátricas,
+                    cuenta con 349 camas, 158 consultorios, 12 quirófanos y 3 terapias intensivas y el desarrollo de la investigación tanto clínica como básica forma parte de nuestra esencia
+                </p>
+            </Page>
+        </Container>
+    )
+}
+
+export default General
