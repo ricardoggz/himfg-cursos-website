4e659b05bd5f14447525e87932a58fbba5c5c21c
ricardoggz <ricardo_ggzicm@outlook.com>
2023-04-20 11:30:31 -0600

    [add]: new page to medica

---
+++ src/pages/medica/index.js
@@ -0,0 +1,25 @@
+import { Page, Container } from '../../components'
+
+const Medica = ()=>{
+    return (
+        <Container>
+            <Page title='Dirección Médica'>
+                <p>
+                    La Dirección Médica coordina el trabajo de 5 subdirecciones: Asistencia Médica, Asistencia Quirúrgica, Recursos Auxiliares y de Diagnóstico, Atención Integral al Paciente y Pediatría Ambulatoria,
+                    cada una con sus departamentos y servicios.
+                    Nuestra misión es proporcionar atención médica de alta especialidad con los más altos estándares de seguridad y calidad.
+                    Para ello contamos con 225 médicos, 790 enfermeras, 63 trabajadoras sociales y 45 trabajadores de relaciones públicas.
+                    En cuanto a infraestructura tenemos 349 camas, 158 consultorios, 12 quirófanos.
+                    El Hospital cuenta con equipo sofisticado de alta tecnología entre los que destacan el tomógrafo de 3 teslas, resonancia magnética, gamacámara, acelerador lineal y el microscopio electrónico de transmisión de 120 kv
+                </p>
+                <p>
+                    Cada año otorgamos más de 200,000 consultas, 16,000 atenciones en urgencias, se realizan más de 5000 procedimientos quirúrgicos de alta especialidad, mas de 11,000 anestesias, 1,000, 000 de estudios de laboratorio y gabinete.
+                    Egresan anualmente de hospitalización alrededor de 7500 pacientes, uno de cada tres con padecimiento oncológico y uno de cada 5 con malformaciones congénitas.
+                    A pesar de lo grave y complejo de las enfermedades que se atienden, la mortalidad es solo de 2.3/100 egresos, la tasa de sobrevida por leucemia es del 85%, cuando en 2012 era del 64%. La percepción de satisfacción en calidad de atención se ubica en 95%
+                </p>
+            </Page>
+        </Container>
+    )
+}
+
+export default Medica
