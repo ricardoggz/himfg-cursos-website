0e349831b67e675bd1db3ef8f184801132ec62ee
ricardoggz <ricardo_ggzicm@outlook.com>
2023-06-19 11:32:30 -0600

    [add]: pdf course

---
+++ src/pages/pdf/index.js
@@ -0,0 +1,61 @@
+import { useContext, useEffect } from "react"
+import { useRouter } from "next/router"
+import jsPDF from "jspdf"
+import html2canvas from "html2canvas"
+import { formatDate } from "@/helpers"
+import { Container, Title } from "@/components"
+import { downloadPdf } from './pdf.module.css'
+import { CourseContext, UserContext } from "@/contexts"
+
+const PDF = ()=>{
+    const router = useRouter()
+    const { course } = useContext(CourseContext)
+    const { user } = useContext(UserContext)
+    const downloadPDF = () =>{
+        const capture = document.querySelector('#downloadPdf');
+            html2canvas(capture).then((canvas)=>{
+            const doc = new jsPDF({
+                orientation: "landscape",
+                unit: "in",
+                format: [12, 12]
+            })
+            doc.text(`
+                Felicidades, has comprado satisfactoriamente el curso:
+                ${course.course_name}
+
+                Datos de curso:
+                Costo: $ ${course.course_price} mxn
+                Fecha de inicio: ${formatDate(course.course_start_date)}
+                Fecha de término: ${formatDate(course.course_finish_date)}
+                URL de curso:
+                https://www.test.him.edu.mx/video/${course.course_url}.html
+                Contraseña: ${course.course_password}
+
+                Datos de estudiante:
+                Nombre: ${user.student_name}
+                Email: ${user.student_email}
+                Contraseña: ${user.student_password}
+            `, 1, 1);
+            doc.save(`${course.course_name}.pdf`);
+            })
+        }
+        useEffect(()=>{
+            if(!course){
+                router.push('/ensenanza/offer')
+            }
+        },[])
+    return (
+        <Container>
+            <div className={`${downloadPdf} flexContainer`} id='downloadPdf'>
+                <Title>Curso: {course.course_name}</Title>
+                <button
+                    onClick={downloadPDF}
+                >
+                    Descargar acceso de curso
+                </button>
+            </div>
+        </Container>
+    )
+}
+
+export default PDF
