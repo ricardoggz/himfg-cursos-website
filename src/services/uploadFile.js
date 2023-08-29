5d18402f9dc5c39e8ec858fa024a73e4e07517fc
ricardoggz <ricardo_ggzicm@outlook.com>
2023-08-29 11:15:16 -0600

    [change]: fetch to axios

--- src/services/uploadFile.js
+++ src/services/uploadFile.js
@@ -1,19 +1,16 @@
+import axios from "axios";
 export const uploadFile = async({file})=>{
   try {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('upload_preset', 'image_himfg');
     formData.append('public_id', file.name)
-    const response = await fetch(
+    const response = await axios.post(
       'https://api.cloudinary.com/v1_1/diuxbqmn5/image/upload',
-      {
-        method: 'POST',
-        body: formData,
-      }
-    );
-  
-    const data = await response.json();
-    return data
+      formData
+    )
+    console.log(response.data.secure_url)
+    return response.data.secure_url
   } catch (error) {
     console.log(error)
   }
