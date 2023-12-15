0375708d7a6636a46ffe40a1c0b20b629d5c076a
ricardoggz <ricardo_ggzicm@outlook.com>
2023-12-14 22:47:13 -0600

    [change]: URL para cifrar datos de pago

--- src/components/paymentForm/cyperData.js
+++ src/components/paymentForm/cyperData.js
@@ -25,7 +25,7 @@ function cypherData(p, key) {
   jQuery.ajax({
     //url: "https://localhost:7080/aes/cifradoconllave", // Desde Visual Studio 2022
     //url : 'http://localhost:5080/aes/cifradoconllave',  // Desde Ejecutable Cifrador.exe
-    url: "https://ricardoguevarag-001-site1.gtempurl.com/aes/cifradoconllave",
+    url: "http://carolinamagos-001-site1.anytempurl.com/aes/cifradoconllave",
     type: "POST",
     async: false,
     headers: {
