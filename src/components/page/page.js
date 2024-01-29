32d4e9a3911e0d0e62e74a9c2afecfedb9183221
ricardoggz <ricardo_ggzicm@outlook.com>
2024-01-29 13:29:30 -0600

    cambios y mas cambios

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,7 +1,9 @@
 import { useState } from "react"
+import Image from "next/image"
 import Link from "next/link"
 import { IoIosArrowDown } from "react-icons/io"
 import { Title } from '../../components'
+import direccion_general from '../../assets/direccion-general.png'
 import styles from "./page.module.css"
 
 export const Page = ({children, title, listMenu, textMenu, route}) => {
@@ -9,6 +11,38 @@ export const Page = ({children, title, listMenu, textMenu, route}) => {
   const showMenu = ()=> setIsOpen(!isOpen)
   return (
     <>
+      <section className={styles.pageBanner}>
+        <div className={styles.pageBannerBackground}>
+          <div className={styles.pageTitleBanner}>
+            <h1>
+              Semblanza Dirección General
+            </h1>
+            <p>
+              El Hospital Infantil de México tiene 78 años brindando atención pediátrica especializada a la niñez más desprotegida de nuestro país,
+              su modelo de atención,
+              enseñanza e investigación,
+              han sido repetidos en los actuales Institutos Nacionales de Salud y otros hospitales del país y de América Latina.
+            </p>
+            <p>
+              Sus recursos provienen en su mayoría de las arcas federales y atenciones subrogadas, sin embargo, no es suficiente.
+            </p>
+            <p>
+              Nuestro Instituto es pionero de pediatría en el país en una gran cantidad de temas del desarrollo y de vanguardia como: vacunas, bacteriología, infectología, nutrición, nefrología, endocrinología, cirugía de corazón, trasplantes de corazón, riñón, etc.
+            </p>
+            <p>
+              Ha sido siempre un icono de la Pediatría Nacional, hemos publicado más de 300 libros; múltiples artículos científicos, capítulos de libros desde hace más de 17 años tenemos el "Programa Sigamos Aprendiendo en el Hospital" que apoya a los pacientes a continuar con sus estudios estando hospitalizados tenemos una historia larga en la formación de alumnos de todas las especialidades pediátricas, cuenta con 349 camas, 158 consultorios, 12 quirófanos y 3 terapias intensivas y el desarrollo de la investigación tanto clínica como básica forma parte de nuestra esencia.
+            </p>
+          </div>
+        </div>
+      </section>
+      <div className={styles.texts}>
+        <figure>
+          <Image src={direccion_general} />
+        </figure>
+        <div className={styles.text}>
+            {children}
+        </div>
+      </div>
       <div className="flexContainer">
           {
             !route ? null
@@ -22,10 +56,6 @@ export const Page = ({children, title, listMenu, textMenu, route}) => {
           }
       </div>
       <br/>
-      <Title>{title}</Title>
-      <div className={`${styles.pagePharagraphs} flexContainer`}>
-        {children}
-      </div>
     </>
   );
 };
--- src/pages/general/index.js
+++ src/pages/general/index.js
@@ -2,31 +2,51 @@ import { Container, Page } from '../../components'
 
 const General = ()=>{
     return (
-        <Container>
-            <Page title='Dirección General'>
+            <Page>
+                <center>
+                    <h3>
+                        Adrián Chávez López
+                    </h3>
+                    <h4>(Director General)</h4>
+                    <br />
+                </center>
                 <p>
-                    El Hospital Infantil de México tiene 78 años brindando atención pediátrica especializada a la niñez más desprotegida de nuestro país,
-                    su modelo de atención,
-                    enseñanza e investigación,
-                    han sido repetidos en los actuales Institutos Nacionales de Salud y otros hospitales del país y de América Latina.
+                    Soy mexicano por nacimiento. 
                 </p>
                 <p>
-                    Sus recursos provienen en su mayoría de las arcas federales y atenciones subrogadas,
-                    sin embargo, no es suficiente.
+                    Mi máximo orgullo es mi familia, Elena, mi esposa y Fabián Emiliano, mi hijo. 
                 </p>
                 <p>
-                    Nuestro Instituto es pionero de pediatría en el País en una gran cantidad de temas del desarrollo y de vanguardia como: vacunas,
-                    bacteriología, infectología, nutrición, nefrología, endocrinología, cirugía de corazón, trasplantes de corazón, riñón, etc.
+                    Mis valores son la honestidad, la lealtad, el espíritu de servicio, el trabajo en equipo y la equidad. 
                 </p>
                 <p>
-                    Ha sido siempre un icono de la Pediatría Nacional,
-                    hemos publicado más de 300 libros;
-                    múltiples artículos científicos,
-                    capítulos de libros desde hace más de 17 años tenemos el "Programa Sigamos Aprendiendo en el Hospital" que apoya a los pacientes a continuar con sus estudios estando hospitalizados tenemos una historia larga en la formación de alumnos de todas las especialidades pediátricas,
-                    cuenta con 349 camas, 158 consultorios, 12 quirófanos y 3 terapias intensivas y el desarrollo de la investigación tanto clínica como básica forma parte de nuestra esencia.
+                    Este es mi historial académico:
+                </p>
+                <p>
+                    Licenciatura en Medicina en la Facultad de Medicina de la Universidad Nacional Autónoma de México, 1988.
+                </p>
+                <p>
+                    Especialidad en Pediatría Médica, Hospital Infantil de México Federico Gómez, División Estudios Posgrado, Facultad de Medicina Universidad Nacional Autónoma de México, 1991. Subespecialidad en Medicina Crítica Pediátrica, HIMFG, SSA, División de Estudios de Posgrado, Facultad de Medicina Universidad Nacional Autónoma de México, 1991. 
+                </p>
+                <p>
+                    Desde 1993 trabajé como médico adscrito al Departamento de Terapia Intensiva Pediátrica del Hospital Infantil de México Federico Gómez. Fui Jefe de Servicio del mismo Departamento de 1993 a 1996. A partir de junio de 1996 y hasta enero 13 de 2024 ocupe el cargo de Jefe de Departamento. 
+                </p>
+                <p>
+                    El 13 de enero del 2024 fui designado Director General del Hospital Infantil de México Federico Gómez por Junta de Gobierno de nuestro hospital. 
+                </p>
+                <p>
+                    Tengo el privilegio de haber sido Profesor de Pediatría Médica (pre grado) y Pediatría y de Medicina Crítica Pediátrica (pos grado) en los Programas de la Facultad de Medicina de la Universidad Nacional Autónoma de México. Igualmente, por decisión de mis pares, fui el Presidente Fundador de la Sección de Medicina Critica Pediátrica del Consejo Mexicano de Certificación en Pediatría A.C. 
+                </p>
+                <p>
+                    He trabajado en muchos lados, pero el único que me ha enamorado es el “Infantil”, donde he colaborado por 35 años ya; durante este tiempo participé en el desarrollo, consolidación y éxito de nuestro servicio, “La Terapia”, que ya desde hace muchos años ha sido la mejor de México, reconocida en América Latina y el mundo. 
+                </p>
+                <p>
+                    Junto con un grupo de colegas, amigos, desarrolle el Proyecto Estratégico Institucional para el periodo 2024 – 2029, este contiene centenares de años de experiencia y vivencias, necesarias y suficientes para proponer una nueva ruta de desarrollo que todos, juntos, vamos a implementar. 
+                </p>
+                <p>
+                    El objetivo del Proyecto es establecer un sistema de gestión hospitalaria horizontal, basado en procesos estandarizados y procedimientos específicos para cada área sustantiva del instituto, cuyas metas son garantizar: a) la atención, seguridad y satisfacción de los usuarios, b) el bienestar, seguridad y desarrollo del personal, c) la sustentabilidad social, administrativa y financiera del instituto, y d) la productividad institucional. 
                 </p>
             </Page>
-        </Container>
     )
 }
 
