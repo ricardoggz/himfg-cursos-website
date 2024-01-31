1e975f41f2253765f701d2adcadeb5c3e2cce2a1
ricardoggz <ricardo_ggzicm@outlook.com>
2024-01-31 14:20:14 -0600

    se crea una pagina para crear rutas dinamicas de las direcciones

---
+++ src/pages/direccion/[...id].js
@@ -0,0 +1,36 @@
+export const getStaticPaths = async()=>{
+    const response = await fetch(`${process.env.BASE_URL_API}api/courses/all-courses`)
+    const json = await response.json()
+    const paths = json.filter((course)=>course.course_vimeo_folder)
+    .map((course)=>{
+        return{
+            params:{
+                 id: [
+                    `${course.course_url}`,
+                ],
+            }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+export const getStaticProps=async({params})=>{
+    const videoId = params.id ?? ''
+    const config = {
+        headers: { Authorization: `Bearer 586637bf90ea7727edc8c90c95b056c3` }
+    }
+    const response = await fetch(
+        `https://api.vimeo.com/me/folders/${videoId[1]}/videos`,
+        config
+    )
+    const data = await response.json()
+    return {
+        props:{
+            data: data,
+            title: videoId[0] || null,
+            liveVideoId: videoId[2] ? videoId[2] : null
+        }
+    }
+}
