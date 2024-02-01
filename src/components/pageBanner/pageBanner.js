b75b93ca08363a6744cd6ed052acfa62d39ab09d
ricardoggz <ricardo_ggzicm@outlook.com>
2024-02-01 14:10:53 -0600

    se instala emotion y se hace el banner responsive

--- src/components/pageBanner/pageBanner.js
+++ src/components/pageBanner/pageBanner.js
@@ -1,54 +1,68 @@
+import { css } from '@emotion/css'
 import { Menu } from '../../components'
 
 
 export const PageBanner = ({title, children, banner}) => {
-    const pageBanner={
-      backgroundImage : `url(${banner})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '50vh',
-      width: '100%'
-    }
-    const pageBannerBackground = {
-      width:'100%',
-      height:'100%',
-      background: 'rgb(152,152,152)',
-      background: 'linear-gradient(90deg, rgba(254,254,254,0) 0%, rgba(255,255,255,0) 38%, rgba(7,46,0,0.999019676229867) 92%)',
-      padding: '3rem 0', 
-    }
-    const pageTitleBanner={
-      textAlign: 'justify',
-      alignItems: 'end',
-      color:'var(--white-primary)',
-      display:'flex',
-      flexDirection: 'column',
-      gap:'1.5rem',
-      justifyContent: 'center',
-      width:'30%',
-      height:'100%',
-      marginLeft:'60%',
-      padding:'3rem',
-      lineHeight:'2rem',
-    }
-    const pageBannerText={
-      overflowX: 'hidden',
-      overflowY: 'scroll',
-    }
+    const pageBanner=css`
+      background:url(${banner});
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      height: 50vh;
+      width: 100%;
+    `
+    const pageBannerBackground=css`
+      width:100%;
+      height:100%;
+      background: rgb(152,152,152);
+      background: linear-gradient(90deg, rgba(254,254,254,0) 0%, rgba(255,255,255,0) 38%, rgba(7,46,0,0.999019676229867) 92%);
+      padding: 3rem 0;
+    `
+    const pageTitleBanner=css`
+      text-align: justify;
+      align-items: end;
+      color:var(--white-primary);
+      display:flex;
+      flex-direction: column;
+      gap:2rem;
+      justify-content: center;
+      width:30%;
+      height:100%;
+      margin-left:60%;
+      padding:3rem;
+      line-height:2rem;
+      text-align:center;
+      @media (min-width:240px) and (max-width:880px){
+        width:100%;
+        align-items:center;
+        margin:0;
+        padding:1rem;
+      }
+    `
+    const pageBannerText=css`
+      text-align:justify;
+      overflow-x: hidden;
+      overflow-y: scroll;
+      
+      @media (min-width:240px) and (max-width:880px){
+        padding:1rem;
+        background:var(--banner-responsive);
+      }
+      `
     return (
       <>
         <section
-          style={pageBanner}
+          className={pageBanner}
         >
           <div
             
-            style={pageBannerBackground}
+            className={pageBannerBackground}
           >
             <div
-              style={pageTitleBanner}
+              className={pageTitleBanner}
             >
               <h1>{title}</h1>
-              <p style={pageBannerText}>
+              <p className={pageBannerText}>
                 {children}
               </p>
             </div>
