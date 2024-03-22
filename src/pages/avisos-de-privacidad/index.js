f482bbbb62a9da2ffeda8f49b0525247985498bc
ricardoggz <ricardo_ggzicm@outlook.com>
2024-03-22 12:11:27 -0600

    se agregan avisos de privacidad

--- src/pages/avisos-de-privacidad/index.js
+++ src/pages/avisos-de-privacidad/index.js
@@ -6,13 +6,91 @@ export default function PrivacityPage(){
         <Container>
             <ul className={styles.privacityWrapper}>
                 <li>
-                    <a href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/Documento_Seguridad_sm.doc'>
-                        Aviso de privacidad Simplificado para el Uso de Estacionamiento en el HIMFG
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/1.pdf'
+                        target='_blank'
+                    >
+                    Avisos de Privacidad Integral del Expediente Clínico 
                     </a>
                 </li>
                 <li>
-                    <a href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/Avisos_datos_sm.doc'>
-                        Documento de Seguridad para la Protección de Datos Personales en Posesión de la Subdirección de Mantenimiento.
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/2.pdf'
+                        target='_blank'
+                    >
+                    Avisos de Privacidad Integral de los Procesos de Recursos Humanos  
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/3.pdf'
+                        target='_blank'
+                    >
+                    Avisos de Privacidad Integral Expediente Social 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/4.pdf'
+                        target='_blank'
+                    >
+                    Avisos de privacidad integral del Sistema de Investigadores 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/5.pdf'
+                        target='_blank'
+                    >
+                    Avisos de Privacidad Integral del Servicio de Banco de Sangre y Medicina Transfusional 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/6.pdf'
+                        target='_blank'
+                    >
+                    Avisos de Privacidad Integral del Registro y evaluación de Residentes 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/7.pdf'
+                        target='_blank'
+                    >
+                    Aviso de Privacidad Integral para acceso a información y Derechos ARCO 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/8.pdf'
+                        target='_blank'
+                    >
+                    Aviso de Privacidad Integral para el Acceso al Comedor Institucional 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/9.pdf'
+                        target='_blank'
+                    >
+                    Aviso de Privacidad Integral para el Uso de Estacionamiento en el HIMFG  
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/10.pdf'
+                        target='_blank'
+                    >
+                    Aviso de privacidad Integral para la Encuesta de Satisfacción del Usuario. Egreso Hospitalario y Consulta Externa. 
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href='https://archivos.him.edu.mx/graficos/avisos-de-privacidad/11.pdf'
+                        target='_blank'
+                    >
+                    Aviso de Privacidad Integral para el Buzón de Quejas, Sugerencias y Felicitaciones
                     </a>
                 </li>
             </ul>
