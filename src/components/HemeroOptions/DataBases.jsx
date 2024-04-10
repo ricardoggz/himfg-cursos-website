5bbcf70183921f45c5bbbb1af809e2e92f0dd4f9
ricardoggz <ricardo_ggzicm@outlook.com>
2024-04-10 10:12:16 -0600

    se cambian las bases de datos

--- src/components/HemeroOptions/DataBases.jsx
+++ src/components/HemeroOptions/DataBases.jsx
@@ -4,6 +4,15 @@ export const DataBases = ()=>{
     return (
         <div className={styles.hemeroList}>
             <a
+                href='https://www.jaypeedigital.com/home'
+                terget='_blank'
+                className={styles.hemeroListImage}
+            >
+                <figure>
+                    <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/jaypee.png'/>
+                </figure>
+            </a>
+            <a
                 href='http://www.clinicalkey.es'
                 terget='_blank'
                 className={styles.hemeroListImage}
@@ -13,39 +22,12 @@ export const DataBases = ()=>{
                 </figure>
             </a>
             <a
-                href='https://conricyt.remotexs.co/user/login?dest=http%3A%2F%2Fapps.webofknowledge.com.conricyt.remotexs.co%2FWOS_GeneralSearch_input.do%3Fproduct%3DWOS%26search_mode%3DGeneralSearch%26SID%3D5CpBAX3E1JFLqOzAZOL%26preferencesSaved%3D%26highlighted_tab%3DWOS'
-                terget='_blank'
-                className={styles.hemeroListImage}
-            >
-                <figure>
-                    <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/db_clarivate.jpeg'/>
-                </figure>
-            </a>
-            <a
-                href='https://conricyt.remotexs.co/user/login?dest=http%3A%2F%2Fwww.sciencedirect.com.conricyt.remotexs.co%2F'
-                terget='_blank'
-                className={styles.hemeroListImage}
-            >
-                <figure>
-                    <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/db_scienceDirect.jpeg'/>
-                </figure>
-            </a>
-            <a
                 href='https://onlinelibrary.wiley.com/'
                 terget='_blank'
                 className={styles.hemeroListImage}
             >
                 <figure>
                     <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/db_wiley.jpeg'/>
-                </figure>
-            </a>
-            <a
-                href='https://sites.google.com/view/consulta-informacion-conricyt/p%C3%A1gina-principal'
-                terget='_blank'
-                className={styles.hemeroListImage}
-            >
-                <figure>
-                    <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/db_ccinshae.jpeg'/>
                 </figure>
             </a>
             <a
@@ -66,16 +48,6 @@ export const DataBases = ()=>{
                     <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/bd_repos.jpeg'/>
                 </figure>
                 <span>Boletín Médico del Hospital Infantil de México</span>
-            </a>
-            <a
-                href='https://www.karger.com/Resources/Qinsight-es'
-                terget='_blank'
-                className={styles.hemeroListImage}
-            >
-                <figure>
-                    <img src='https://archivos.him.edu.mx/graficos/ensenanza/hemerobiblioteca/db_karger.jpeg'/>
-                </figure>
-                <span>Pediatrics in Review</span>
             </a>
         </div>
     )
