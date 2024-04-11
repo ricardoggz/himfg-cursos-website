400861c62077ea0b4bff7c53b536720f3e01d4f0
ricardoggz <ricardo_ggzicm@outlook.com>
2024-04-11 08:44:24 -0600

    se agregan los documentos faltantes

--- src/components/FileList/const.js
+++ src/components/FileList/const.js
@@ -24,41 +24,29 @@ export const infoContable = [
         title: 2022,
         content:[
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-1.pdf',
-                title:'Estados financieros 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2022/2022-1.pdf',
+                title:'Estados financieros 2022'
             },
-            {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-2.pdf',
-                title: 'Pasivos Contingentes a Septiembre 2023'
-            },
-            {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-3.pdf',
-                title: 'Notas a los Estados Financieros al 30 de Septiembre de 2023'
-            },
-            {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-4.pdf',
-                title: 'Estados Financieros de Cuenta Pública 2023'
-            }
         ]
     },
     {
         title: 2021,
         content:[
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-1.pdf',
-                title:'Estados financieros 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2021/2021-1.pdf',
+                title:'Estados financieros 1 2021'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-2.pdf',
-                title: 'Pasivos Contingentes a Septiembre 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2021/2021-2.pdf',
+                title: 'Estados financieros 2 2021'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-3.pdf',
-                title: 'Notas a los Estados Financieros al 30 de Septiembre de 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2021/2021-3.pdf',
+                title: 'Estados financieros 3 2021'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-4.pdf',
-                title: 'Estados Financieros de Cuenta Pública 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2021/2021-4.pdf',
+                title: 'Estados financieros 4 2021'
             }
         ]
     },
@@ -66,20 +54,20 @@ export const infoContable = [
         title: 2020,
         content:[
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-1.pdf',
-                title:'Estados financieros 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2020/2020-1.pdf',
+                title:'Estados financieros 1 2020'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-2.pdf',
-                title: 'Pasivos Contingentes a Septiembre 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2020/2020-2.pdf',
+                title: 'Estados financieros 2 2020'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-3.pdf',
-                title: 'Notas a los Estados Financieros al 30 de Septiembre de 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2020/2020-3.pdf',
+                title: 'Estados financieros 3 2020'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-4.pdf',
-                title: 'Estados Financieros de Cuenta Pública 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2020/2020-4.pdf',
+                title: 'Estados financieros 4 2020'
             }
         ]
     },
@@ -87,20 +75,20 @@ export const infoContable = [
         title: 2019,
         content:[
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-1.pdf',
-                title:'Estados financieros 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2019/2019-1.pdf',
+                title:'Estados financieros 1 2019'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-2.pdf',
-                title: 'Pasivos Contingentes a Septiembre 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2019/2019-2.pdf',
+                title: 'Estados financieros 2 2019'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-3.pdf',
-                title: 'Notas a los Estados Financieros al 30 de Septiembre de 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2019/2019-3.pdf',
+                title: 'Estados financieros 3 2019'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-4.pdf',
-                title: 'Estados Financieros de Cuenta Pública 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2019/2019-4.pdf',
+                title: 'Estados financieros 4 2019'
             }
         ]
     },
@@ -108,20 +96,20 @@ export const infoContable = [
         title: 2018,
         content:[
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-1.pdf',
-                title:'Estados financieros 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2018/2018-1.pdf',
+                title:'Estados financieros 1 2018'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-2.pdf',
-                title: 'Pasivos Contingentes a Septiembre 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2018/2018-2.pdf',
+                title: 'Estados financieros 2 2018'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-3.pdf',
-                title: 'Notas a los Estados Financieros al 30 de Septiembre de 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2018/2018-3.pdf',
+                title: 'Estados financieros 3 2018'
             },
             {
-                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2023/2023-4.pdf',
-                title: 'Estados Financieros de Cuenta Pública 2023'
+                link: 'https://archivos.him.edu.mx/graficos/administracion/subdireccion-recursos-financieros/informacion-contable/2018/2018-4.pdf',
+                title: 'Estados financieros 4 2018'
             }
         ]
     }
