68e00aec34a5f07e329b1a353f486ecab5679b97
ricardoggz <ricardo_ggzicm@outlook.com>
2024-04-23 12:58:48 -0600

    se agrega el listado

--- src/components/EducativeOffer/EducativeOffer.jsx
+++ src/components/EducativeOffer/EducativeOffer.jsx
@@ -5,7 +5,29 @@ export const EducativeOffer = ()=>{
         <div className={styles.educativeOfferWrapper}>
             <figure>
                 <img src='https://archivos.him.edu.mx/graficos/ensenanza/pre-y-posgrado/oferta-academica.png'/>    
-            </figure>            
+            </figure>
+            <div className={`flexContainer ${styles.adsWrapper}`}>
+                <div>
+                    <span className={styles.adTitle}>
+                        Pre grado
+                    </span>
+                    <ul className={styles.adList}>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                    </ul>
+                </div>
+                <div>
+                <span className={styles.adTitle}>Pos grado</span>
+                <ul className={styles.adList}>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                        <li>Opción 1</li>
+                </ul>
+                </div>
+            </div>            
         </div>
     )
 }
