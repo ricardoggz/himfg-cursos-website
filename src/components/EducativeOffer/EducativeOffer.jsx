b1a030ad72d4bd9b4258703835797209e7832e5f
ricardoggz <ricardo_ggzicm@outlook.com>
2024-05-13 12:51:11 -0600

    se agrega enlaces a inscripciones

--- src/components/EducativeOffer/EducativeOffer.jsx
+++ src/components/EducativeOffer/EducativeOffer.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { Title } from '../title/title'
 import styles from './educativeoffer.module.css'
 
 export const EducativeOffer = ()=>{
@@ -7,6 +8,7 @@ export const EducativeOffer = ()=>{
             <figure>
                 <img src='https://archivos.him.edu.mx/graficos/ensenanza/pre-y-posgrado/oferta-academica.png'/>    
             </figure>
+            <h2>Realiza tu registro</h2>
             <div className={`flexContainer ${styles.adsWrapper}`}>
                 <div>
                     <Link
@@ -16,17 +18,30 @@ export const EducativeOffer = ()=>{
                         Pre grado
                     </Link>
                     <ul className={styles.adList}>
-                        
+                        <li>
+                        <Link
+                        href='/inscripcion-pre-y-posgrado/pregrado'
+                        >
+                            Registro
+                        </Link>
+                        </li>
                     </ul>
                 </div>
                 <div>
                     <Link
                         className={styles.adTitle}
-                        href='inscripcion-pre-y-posgrado/posgrado'
+                        href='/inscripcion-pre-y-posgrado/posgrado'
                     >
                         Pos grado
                     </Link>
                 <ul className={styles.adList}>
+                        <li>
+                            <Link
+                                href='/inscripcion-pre-y-posgrado/posgrado'
+                            >
+                                Registro
+                            </Link>
+                        </li>
                         <li>
                             <a
                                 href='https://archivos.him.edu.mx/inscripciones-pos-grado/reglamento_residentes.pdf'
