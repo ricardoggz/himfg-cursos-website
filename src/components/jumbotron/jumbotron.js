761436542beeef2696738a84087aa39310f678cf
himfg <himfgence@gmail.com>
2024-11-11 12:22:35 -0600

    se cambia el banner y se elimina la carpeta zip

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -17,7 +17,7 @@ export const Jumbotron =()=>{
         }
         const images = [
             {
-                url:'https://himfg.edu.mx/archivos/banners/banner-4.jpg',
+                url:'https://himfg.edu.mx/archivos/banners/banner-4.webp',
                 link:'/'
             },
             {
