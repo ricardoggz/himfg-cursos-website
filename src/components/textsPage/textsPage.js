9bd8cdcf58201ae56bb29a20be0418f64a340782
himfg <himfgence@gmail.com>
2025-04-24 14:22:31 -0600

    se crea la validacion de estilos para las paginas

--- src/components/textsPage/textsPage.js
+++ src/components/textsPage/textsPage.js
@@ -1,13 +1,20 @@
+'use client'
 import Link from 'next/link'
-import { Title } from '../title/title'
+import { useState, useEffect } from 'react'
 import styles from './textsPage.module.css'
 import { PagePortrait } from '../pagePortrait/pagePortrait'
 
 export const TextsPage = ({ page }) => {
+    const [list, setList] = useState(null)
+    useEffect(() => {
+        if (page.page_staff && page.tipo_departamento === 2) {
+            setList(JSON.parse(page.page_staff))
+        }
+    }, [])
     return (
-        <section className={styles.directionWrapper}>
-            <div className={`${styles.texts} ${styles.textsBackground}`}>
-                <Title>{page.page_title}</Title>
+        <section className={`${page.tipo_departamento !== 2 ? styles.textsWrapper : styles.directionWrapper}`}>
+            <div className={`${styles.texts} ${page.tipo_departamento !== 2 ? 'flexContainer' : styles.textsBackground}`}>
+                <h1>{page.page_title}</h1>
                 <PagePortrait
                     image={page.page_titular_photo}
                     titular={page.page_titular}
@@ -22,18 +29,40 @@ export const TextsPage = ({ page }) => {
                     </p>
                 </div>
                 {
-                    /*<div className={`${styles.text_image}`}>
-                        <img
-                            src={page.page_first_image}
-                            loading='lazy'
-                        />
-                        <img
-                            src={page.page_second_image}
-                            loading='lazy'
-                        />
-                    </div>*/
+
+                    page.tipo_departamento !== 2 ?
+                        <div className={`${styles.text_image}`}>
+                            <img
+                                src={page.page_first_image}
+                                loading='lazy'
+                            />
+                            <img
+                                src={page.page_second_image}
+                                loading='lazy'
+                            />
+                        </div>
+                        :
+                        null
                 }
             </div>
+            {
+                page.tipo_departamento !== 2 ?
+                    null :
+                    <div className={styles.directionOptions}>
+                        {
+                            list && list.map((item, i) => (
+                                <>
+                                    <Link
+                                        href={`${item.link}`}
+                                        key={i}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                </>
+                            ))
+                        }
+                    </div>
+            }
         </section>
     )
 }
